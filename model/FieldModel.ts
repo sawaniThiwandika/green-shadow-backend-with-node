03d19e9945ad94121436c7bf459f1024f2815569
Sawani Thiwandika <thiwandika.whs02@gmail.com>
2025-02-02 23:15:32 +0530

    create fieldModel interface

---
+++ model/FieldModel.ts
@@ -0,0 +1,11 @@
+export interface Field{
+    fieldCode: string;
+    fieldName: string;
+    fieldLocation: string;
+    fieldSize: string;
+    fieldImage1: string;
+    crops: string;//
+    staff: string[];
+    equipment: string[];
+    log:string[];
+}
