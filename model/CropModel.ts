adb913ab5d923718628f945dd1322dcf156adcc0
Sawani Thiwandika <thiwandika.whs02@gmail.com>
2025-02-02 23:15:01 +0530

    create cropModel interface

---
+++ model/CropModel.ts
@@ -0,0 +1,9 @@
+export interface Crop{
+    cropCode: string;
+    commonName: string;
+    scientificName: string;
+    image: string;
+    category: string;
+    season: string;
+    fieldDetails: string [];
+}
