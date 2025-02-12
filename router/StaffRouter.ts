2a0b43a8461654cbc5d0c6e232448a532dd42c64
Sawani Thiwandika <thiwandika.whs02@gmail.com>
2025-02-12 20:14:34 +0530

    add getAll staff endpoint in staff router

--- router/StaffRouter.ts
+++ router/StaffRouter.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import {Staff} from "../model/StaffModel";
-import {addStaff} from "../database/staff-service";
-
+import {addStaff, getAllStaff} from "../database/staff-service";
 
 const router = express.Router();
 
@@ -9,11 +8,21 @@ router.post("/add", async(req, res) => {
     const staff: Staff= req.body;
     try{
         const addedMem = await addStaff(staff);
-        res.send('staff Added')
+        res.send('staff Added');
     }catch(err){
         console.log("error adding staff", err);
         res.status(400).send("error adding staff");
     }
-})
+});
+
+router.get("/getAll", async (req, res) => {
+    try{
+        const staffList=  await getAllStaff();
+        res.json(staffList);
+    }catch(err){
+        console.log("error getting staff list", err);
+    }
+});
+
 
 export default router;
