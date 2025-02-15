a7ccac44ce8899a75b125b245b5b4987c8a70369
Sawani Thiwandika <thiwandika.whs02@gmail.com>
2025-02-15 23:17:41 +0530

    create addLog function

---
+++ database/log-service.ts
@@ -0,0 +1,40 @@
+import {PrismaClient} from "@prisma/client";
+
+import {Log} from "../model/LogModel";
+
+const prisma = new PrismaClient();
+
+
+export async function addLog(log: Log) {
+    try {
+        if (!log.relevantStaff || log.relevantStaff.length === 0) {
+            throw new Error("Staff information is required to create a staff log.");
+        }
+
+        const addedLog = await prisma.log.create({
+            data: {
+                logCode: log.logCode,
+                logDate: log.logDate,
+                logDetails: log.logDetails,
+                observedImage: log.observedImage,
+                field: {
+                    connect: { fieldCode: log.relevantFields }
+                },
+                crop: {
+                    connect: { cropCode: log.relevantCrops}
+                },
+                staffLogs: {
+                    create: log.relevantStaff.map((staffId: string) => ({
+                        staff: { connect: { staffId } }
+                    }))
+                }
+            },
+        });
+
+        console.log("Log created:", addedLog);
+        return addedLog;
+    } catch (error) {
+        console.error("Error adding log:", error);
+        throw new Error("Could not add log: " + error);
+    }
+}
